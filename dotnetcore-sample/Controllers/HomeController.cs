5987368434d6fd4afe51c0c0d50bfafa538a79c1
mlhoop <mlhoop@live.com>
2017-09-20 11:30:47 -0500

    simple comment

--- dotnetcore-sample/Controllers/HomeController.cs
+++ dotnetcore-sample/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetcore_sample.Controllers
-{
+{//test
     public class HomeController : Controller
     {
         public IActionResult Index()
